bcd7016b590614518d1195360836dbb1593b4726
Jordan-p20 <71034120+Jordan-p20@users.noreply.github.com>
2023-05-23 16:10:08 -0400

    can interrupt attacks with roll, can move within roll

--- HamsterGame/Assets/Scripts/PlayerController/PlayerStateMachineStates/PlayerStateRoll.cs
+++ HamsterGame/Assets/Scripts/PlayerController/PlayerStateMachineStates/PlayerStateRoll.cs
@@ -14,14 +14,14 @@ public class PlayerStateRoll : PlayerState
 
     private const float FAST_ROLL_SPEED = 6.5f;
     private const float SLOW_ROLL_SPEED = 2.5f;
-    private Vector3 directionVector;
+    private const float BLEND_TIME = 5f;
+
     private float animLength;
     private float elapsedTime = 0f;
     public override void OnStateEnter()
     {
         anim.SetBool("Roll", true);
         animLength = animLengths["DodgeRoll"];
-        playerBody.rotation = Quaternion.LookRotation(directionVector, Vector3.up);
     }
 
     public override void OnStateExit()
@@ -33,10 +33,38 @@ public class PlayerStateRoll : PlayerState
     {
         elapsedTime += Time.deltaTime;
 
-        //if (elapsedTime < 0.3667f)
-        //{
-        //    rollSpeed = SLOW_ROLL_SPEED;
-        //}
+        SelectMoveSpeed();
+
+        GetHorizontalMotion();
+
+        RotateBody();
+
+        controller.Move( ( (horizontalMotion * rollSpeed) + verticalMotion ) * Time.deltaTime );
+    }
+
+    private void RotateBody()
+    {
+        playerBody.rotation = Quaternion.Lerp(playerBody.rotation, Quaternion.LookRotation(horizontalMotion, Vector3.up), Time.deltaTime * BLEND_TIME);
+    }
+
+    private void GetHorizontalMotion()
+    {
+        Vector2 moveInput = PlayerManager.playerControllerInput.moveInput;
+        Vector3 forward = PlayerManager.playerCameraMovement.GetDirectionVector(CamDirection.FORWARD);
+
+        Vector3 right = PlayerManager.playerCameraMovement.GetDirectionVector(CamDirection.RIGHT);
+
+        Vector3 input = (((forward * moveInput.y) + (right * moveInput.x)).normalized);
+
+        if (input.sqrMagnitude != 0)
+        {
+            horizontalMotion = input;
+        }
+        
+    }
+
+    private void SelectMoveSpeed()
+    {
         if (elapsedTime <= 0.8667f)
         {
             rollSpeed = FAST_ROLL_SPEED;
@@ -49,12 +77,22 @@ public class PlayerStateRoll : PlayerState
         {
             rollSpeed = 0;
         }
-
-        controller.Move( ( (directionVector * rollSpeed) + verticalMotion ) * Time.deltaTime );
     }
 
     public override void TransitionCheck()
     {
+        if (!controller.isGrounded)
+        {
+            SM.TransitionState(PlayerStates.FALL);
+            return;
+        }
+
+        if (elapsedTime >= animLength * 0.78f && PlayerManager.playerControllerInput.attackPressed)
+        {
+            SM.TransitionState(PlayerStates.ATTACK);
+            return;
+        }
+
         if (elapsedTime >= animLength * 0.78f)
         {
             SM.TransitionState(PlayerStates.WALK);
@@ -65,10 +103,10 @@ public class PlayerStateRoll : PlayerState
     public override void Initialize(PlayerStateMachineData SMData, Vector3 previousStateHorMotion)
     {
         base.Initialize(SMData, previousStateHorMotion);
-        directionVector = previousStateHorMotion.normalized;
-        if (directionVector.sqrMagnitude <= 0)
+        horizontalMotion = previousStateHorMotion.normalized;
+        if (horizontalMotion.sqrMagnitude <= 0)
         {
-            directionVector = PlayerManager.playerCameraMovement.GetDirectionVector(CamDirection.FORWARD);
+            horizontalMotion = PlayerManager.playerCameraMovement.GetDirectionVector(CamDirection.FORWARD);
         }
 
         verticalMotion = Vector3.up * GRAVITY * 0.15f;
